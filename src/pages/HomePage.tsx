df410ad424d6a734eae7ec040b3d6c8384e27e9e
Elijah Apreko <elijahapreko@gmail.com>
2024-07-11 04:39:39 +0000

    add Home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,21 @@
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Services from "../components/Services";
+import Projects from "../components/Projects";
+import Testimonials from "../components/Testimonials";
+// import Plans from "../components/Plans";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Services />
+      <Projects />
+      <Testimonials />
+      {/* <Plans /> */}
+    </>
+  );
+};
+
+export default HomePage;
