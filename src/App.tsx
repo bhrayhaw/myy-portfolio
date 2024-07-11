241176fa58cad686969b839d98895d0741c28800
Elijah Apreko <elijahapreko@gmail.com>
2024-07-11 04:42:21 +0000

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-// src/App.tsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
+// import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
