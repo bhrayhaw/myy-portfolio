3cf66bbea63af0b17b7a50a91ac46cdd646857eb
Elijah Apreko <elijahapreko@gmail.com>
2024-07-11 04:37:14 +0000

    add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+// src/components/Footer.tsx
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      <div className="container mx-auto">
+        <p>&copy; 2024 Elijah Apreko. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
