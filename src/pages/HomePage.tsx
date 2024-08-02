2bca88164195953a0396e372a575a6663fefa312
Elijah Apreko <elijahapreko@gmail.com>
2024-08-02 15:11:07 +0000

    refactor: removed unused components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,9 @@
 import Hero from "../components/Hero";
-import About from "../components/About";
-import Services from "../components/Services";
-import Projects from "../components/Projects";
-import Testimonials from "../components/Testimonials";
-// import Plans from "../components/Plans";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      {/* <About /> */}
-      {/* <Services /> */}
-      {/* <Projects /> */}
-      {/* <Testimonials /> */}
-      {/* <Plans /> */}
     </>
   );
 };
